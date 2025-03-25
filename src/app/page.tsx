4d95b68be4fd7427b288baf84cd2edeba02c5302
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-25 16:31:22 -0300

    fix: corrigindo comunicação com a stripe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import { IProduct } from "@/types/product";
 import HomeClient from "./home-client";
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
 
 export default async function Home() {
-  const response = await fetch("http://localhost:3000/api/products", {
-    method: "GET",
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
   })
 
-  const data: { products: IProduct[] } = await response.json()
+  const products: IProduct[] = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
 
-  return <HomeClient products={data.products} />
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format((price.unit_amount || 0) / 100)
+    }
+  })
+
+  return <HomeClient products={products} />
 }
