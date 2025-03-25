847824241bfc01c5794eadfd382c88bbbf737859
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-25 17:21:28 -0300

    feat: adicionando revalidação de dados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@ import HomeClient from "./home-client";
 import Stripe from "stripe";
 import { stripe } from "@/lib/stripe";
 
+export const metadata = {
+  revalidate: 60 * 60 * 2 // Revalidação a cada 2 horas
+};
+
 export default async function Home() {
   const response = await stripe.products.list({
     expand: ['data.default_price']
