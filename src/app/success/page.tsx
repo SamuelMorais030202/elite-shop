c3d5248678205d22866d05d8ebf3dd6b979fbc2b
Samuel Morais030202 <moraissamuel009@gmail.com>
2025-03-27 15:34:52 -0300

    feat: criando layout da página de sucesso

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,22 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <div>
-      <h1>Página de sucesso</h1>
-    </div>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuu <strong></strong>, sua <strong></strong> já está a caminho da sua casa
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
